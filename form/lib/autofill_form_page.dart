512ef87304c2912e6c964623cac83e3ecc128f97
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-16 22:21:57 +0900

    AutofillFormPageクラスを作成

---
+++ form/lib/autofill_form_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AutofillFormPage extends StatelessWidget {
+  const AutofillFormPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'パスワード'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
