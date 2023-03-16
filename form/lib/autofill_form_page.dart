604d3b22fd4112ee92b26426673660e1b2d66811
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-16 22:31:20 +0900

    複数のTextFormFieldをAutofillGroupで囲む

--- form/lib/autofill_form_page.dart
+++ form/lib/autofill_form_page.dart
@@ -8,18 +8,20 @@ class AutofillFormPage extends StatelessWidget {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'ユーザーID'),
-              autofillHints: const [AutofillHints.username],
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'パスワード'),
-              autofillHints: const [AutofillHints.password],
-            ),
-          ],
+        child: AutofillGroup(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'ユーザーID'),
+                autofillHints: const [AutofillHints.username],
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'パスワード'),
+                autofillHints: const [AutofillHints.password],
+              ),
+            ],
+          ),
         ),
       ),
     );
