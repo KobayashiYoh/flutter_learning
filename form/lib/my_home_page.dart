8aa1322242d3f31503af350212796d577e1a2d3e
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-16 22:21:21 +0900

    MyHomePageクラスを作成

---
+++ form/lib/my_home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Autofill Form Page'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
