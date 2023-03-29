0331d3d4d0e217245a9cb72ec92249c02b4335b2
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-29 21:12:08 +0900

    TableCalendarを表示

--- calendar/lib/calendar_page.dart
+++ calendar/lib/calendar_page.dart
@@ -1,17 +1,33 @@
+import 'package:calendar/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: TableCalendar(
-          focusedDay: DateTime.now(),
-          firstDay: DateTime.now(),
-          lastDay: DateTime.now(),
+          focusedDay: kToday,
+          firstDay: kFirstDay,
+          lastDay: kLastDay,
+          calendarFormat: _calendarFormat,
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
         ),
       ),
     );
---
+++ calendar/lib/utils.dart
@@ -0,0 +1,3 @@
+final kToday = DateTime.now();
+final kFirstDay = DateTime(1900, 1, 1);
+final kLastDay = DateTime(2099, 12, 31);
