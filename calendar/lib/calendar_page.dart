4e7d968133a3983b07bd041374e8f3747b3aa06d
KobayashiYoh <y.kobayashi@4nonome.com>
2023-03-29 22:25:52 +0900

    日付選択機能を追加

--- calendar/lib/calendar_page.dart
+++ calendar/lib/calendar_page.dart
@@ -11,22 +11,39 @@ class CalendarPage extends StatefulWidget {
 
 class _CalendarPageState extends State<CalendarPage> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = kToday;
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: TableCalendar(
-          focusedDay: kToday,
           firstDay: kFirstDay,
           lastDay: kLastDay,
+          focusedDay: _focusedDay,
           calendarFormat: _calendarFormat,
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            }
+          },
           onFormatChanged: (format) {
             if (_calendarFormat != format) {
               setState(() {
                 _calendarFormat = format;
               });
             }
+          },
+          onPageChanged: (focusedDay) {
+            // No need to call `setState()` here
+            _focusedDay = focusedDay;
           },
         ),
       ),
