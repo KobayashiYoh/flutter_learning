ebda583a433d0842bb2df98e53581cda0ad5cea8
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-23 15:35:21 +0900

    Revert "意図的にUnit testを改ざん"
    This reverts commit 81bfa1a917d90219872fc4d75e152e177c3768e8.

--- github_actions/test/leap_year_test.dart
+++ github_actions/test/leap_year_test.dart
@@ -40,7 +40,7 @@ void main() {
       expect(LeapYear().isLeapYear(2099), false);
     });
     test('value in 2100', () {
-      expect(LeapYear().isLeapYear(2100), true);
+      expect(LeapYear().isLeapYear(2100), false);
     });
     test('value in 2101', () {
       expect(LeapYear().isLeapYear(2101), false);
