deb7dce2ace634182d3603d6dfd20e2f576db072
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-23 15:29:25 +0900

    意図的にテストコードを改ざん

--- github_actions/lib/leap_year.dart
+++ github_actions/lib/leap_year.dart
@@ -1,10 +1,6 @@
 class LeapYear {
   bool isLeapYear(int year) {
-    if (year % 400 == 0) {
-      return true;
-    } else if (year % 100 == 0) {
-      return false;
-    } else if (year % 4 == 0) {
+    if (year % 4 == 0) {
       return true;
     } else {
       return false;
