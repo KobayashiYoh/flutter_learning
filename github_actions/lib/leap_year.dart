6b59bc65b25801bb7176606671c9e02517c9c6ca
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-23 15:30:24 +0900

    Revert "意図的にテストコードを改ざん"
    This reverts commit deb7dce2ace634182d3603d6dfd20e2f576db072.

--- github_actions/lib/leap_year.dart
+++ github_actions/lib/leap_year.dart
@@ -1,6 +1,10 @@
 class LeapYear {
   bool isLeapYear(int year) {
-    if (year % 4 == 0) {
+    if (year % 400 == 0) {
+      return true;
+    } else if (year % 100 == 0) {
+      return false;
+    } else if (year % 4 == 0) {
       return true;
     } else {
       return false;
