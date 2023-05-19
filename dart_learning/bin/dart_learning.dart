145486a0c6d3c8643247b306a42f0bd7182278ae
KobayashiYoh <y.kobayashi@4nonome.com>
2023-05-19 09:20:10 +0900

    create project

---
+++ dart_learning/bin/dart_learning.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+void main() {
+  print('整数を入力してください。');
+  int number = int.parse(stdin.readLineSync()!);
+}
---
+++ fvm/android/app/src/main/kotlin/com/example/fvm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fvm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
