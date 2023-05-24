7cd919da571012f7e1c3a8e83c2a4078f3460099
KobayashiYoh <y.kobayashi@4nonome.com>
2023-05-24 11:19:19 +0900

    main関数内でFirebaseを初期化

--- firebase_authentication/lib/main.dart
+++ firebase_authentication/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_authentication/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
