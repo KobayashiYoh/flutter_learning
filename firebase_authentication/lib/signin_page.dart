e538f6ea8e74a1455ca35e0af3ac422bc46990ef
KobayashiYoh <y.kobayashi@4nonome.com>
2023-05-24 11:59:18 +0900

    メールアドレスとパスワードを用いた新規登録機能を追加

--- firebase_authentication/lib/signin_page.dart
+++ firebase_authentication/lib/signin_page.dart
@@ -1,13 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SigninPage extends StatelessWidget {
+class SigninPage extends StatefulWidget {
   const SigninPage({Key? key}) : super(key: key);
+
+  @override
+  State<SigninPage> createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+  String _inputEmailAddress = '';
+  String _inputPassword = '';
+  String _resultMessage = '';
+
+  void _onChangedEmailAddress(String value) {
+    setState(() {
+      _inputEmailAddress = value;
+    });
+  }
+
+  void _onChangedPassword(String value) {
+    setState(() {
+      _inputPassword = value;
+    });
+  }
+
+  void _onPressedSubmitButton() async {
+    try {
+      final FirebaseAuth auth = FirebaseAuth.instance;
+      final UserCredential result = await auth.createUserWithEmailAndPassword(
+        email: _inputEmailAddress,
+        password: _inputPassword,
+      );
+      final User user = result.user!;
+      setState(() {
+        _resultMessage = '登録完了!\nようこそ、${user.email}';
+      });
+    } catch (e) {
+      setState(() {
+        _resultMessage = '登録失敗';
+      });
+      throw Exception(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign in'),
+        title: const Text('Sign up'),
       ),
       body: SafeArea(
         child: Container(
@@ -15,16 +56,19 @@ class SigninPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(_resultMessage),
               TextFormField(
                 decoration: const InputDecoration(hintText: 'メールアドレス'),
+                onChanged: _onChangedEmailAddress,
               ),
               TextFormField(
                 decoration: const InputDecoration(hintText: 'パスワード'),
+                onChanged: _onChangedPassword,
               ),
               const SizedBox(height: 40.0),
               ElevatedButton(
-                onPressed: () {},
-                child: const Text('ログインする'),
+                onPressed: _onPressedSubmitButton,
+                child: const Text('新規登録'),
               ),
             ],
           ),
