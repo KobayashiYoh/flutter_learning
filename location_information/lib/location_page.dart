e07ac5104b737e123838aad36477dcbcea480724
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-11-22 17:02:53 +0900

    LocationUtilクラスを追加

--- location_information/lib/location_page.dart
+++ location_information/lib/location_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
-import 'package:location_information/location_data_extension.dart';
+import 'package:location_information/location_util.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({Key? key}) : super(key: key);
@@ -12,43 +12,21 @@ class LocationPage extends StatefulWidget {
 class _LocationPageState extends State<LocationPage> {
   LocationData _locationData = LocationData.fromMap({});
 
-  void _updateLocation() async {
-    Location location = Location();
-    final locationData = await location.getLocation();
+  void _setLocation(LocationData data) {
     setState(() {
-      _locationData = locationData;
+      _locationData = data;
     });
   }
 
-  Future<void> _initLocation() async {
-    bool serviceEnabled;
-    PermissionStatus permissionGranted;
-    Location location = Location();
-    location.onLocationChanged.listen((LocationData currentLocation) {
-      _updateLocation();
-    });
-    serviceEnabled = await location.serviceEnabled();
-    if (!serviceEnabled) {
-      serviceEnabled = await location.requestService();
-      if (!serviceEnabled) {
-        return;
-      }
-    }
-    permissionGranted = await location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-    _updateLocation();
+  void _locationListener(LocationData data) {
+    _setLocation(data);
   }
 
   @override
   void initState() {
     super.initState();
     Future(() async {
-      await _initLocation();
+      await LocationUtil.initLocation(_locationListener);
     });
   }
 
@@ -62,16 +40,9 @@ class _LocationPageState extends State<LocationPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('緯度: ${_locationData.latitude?.toStringAsFixed(4)}'),
-            Text('経度: ${_locationData.longitude?.toStringAsFixed(4)}'),
-            Text('標高: ${_locationData.altitude?.toStringAsFixed(2)}（m）'),
-            Text(
-              '速度: ${_locationData.kilometerPerHour.toStringAsFixed(2)}（km/h）',
-            ),
-            Text(
-              'デバイスの水平方向の移動方向: ${_locationData.heading?.toStringAsFixed(2)}（°）',
-            ),
-            Text('isMock: ${_locationData.isMock}'),
+            Text('緯度: ${_locationData.latitude}'),
+            Text('経度: ${_locationData.longitude}'),
+            Text('標高: ${_locationData.altitude}'),
           ],
         ),
       ),
